22d1a8bc5fc551dd127ef7e33cebad70fb0f0f02
sesshomariu <sesshotv@t-online.de>
2016-06-23 22:49:28 +0200

    Added a cleanup function

--- adv_arr.rb
+++ adv_arr.rb
@@ -2,16 +2,23 @@ class Array
 	def contains?(thing)
 		self.each do |i|
 			if i == thing
-			return true
+				return true
 			end
 		end
 		return false
 	end
 
 	def remove(thing)
-		!(self.is_a? Array) ? false : ()
 		a_r = Array.new
-		a.each{|i|i!=n ? a_r<<i : ()}
+		a.each{|i| i!=n ? a_r << i : ()}
+		return a_r
+	end
+
+	def cleanup
+		a_r = Array.new
+		self.each do |x|
+			!(a_r.contains? x) ? a_r << x : ()
+		end
 		return a_r
 	end
 end
